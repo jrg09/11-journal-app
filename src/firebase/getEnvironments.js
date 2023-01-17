26467b01c9e04ac846266c25d8e11e32e36d0e23
Jorge Castro <jorge.files@gmail.com>
2023-01-16 19:20:21 -0600

    log meta

--- src/firebase/getEnvironments.js
+++ src/firebase/getEnvironments.js
@@ -1,6 +1,7 @@
 export const getEnvironments = () => {
   // console.log(process.env);
-  import.meta.env;
+  console.log(import.meta.env);
+  import.meta?.env;
 
   return {
     ...import.meta.env,
